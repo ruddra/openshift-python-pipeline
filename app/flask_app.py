5c03ce1144535c49450640f944411080e56f1b3f
Arnab Kumar Shil <arnab.shil@nhst.com>
2020-01-09 17:54:28 +0600

    updated flask application

--- app/flask_app.py
+++ app/flask_app.py
@@ -2,21 +2,20 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-tasks = [
-    {
-        'id': 1,
-        'title': 'Openshift Jenkins Pipeline Python/Nginx Implementation',
-        'description': 'Find the implementation at https://github.com/ruddra/openshift-python-nginx'
-    },
-    {
-        'id': 2,
-        'title': 'Openshift Jenkins Pipeline Django Implementation',
-        'description': 'Find the implementation at https://github.com/ruddra/openshift-django'
-    }
-]
-
 @app.route('/', methods=['GET'])
 def get_tasks():
+    tasks = [
+        {
+            'id': 1,
+            'title': 'Openshift Jenkins Pipeline Python/Nginx Implementation',
+            'description': 'Find the implementation at https://github.com/ruddra/openshift-python-nginx'
+        },
+        {
+            'id': 2,
+            'title': 'Openshift Jenkins Pipeline Django Implementation',
+            'description': 'Find the implementation at https://github.com/ruddra/openshift-django'
+        }
+    ]
     return jsonify({'tasks': tasks})
 
 if __name__ == '__main__':
